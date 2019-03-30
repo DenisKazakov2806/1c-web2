3e23509a8a9634f8759c644881962305779e4006
DenisKazakov2806 <nextikden@yandex.ru>
2019-03-30 14:06:20 +0300

    instagram1.0.2

---
+++ seminars/instagram/client/components/register.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="page">
+        <div class="focus">
+            <form class="focus_holder" v-on:keydown.enter="register">
+                <input type="text" class="form-control" placeholder="Login" v-model="username">
+                 <input type="password" class="form-control" placeholder="Password" v-model="passwordA">
+                  <input type="password" class="form-control" placeholder="Password again" v-model="passwordB">
+                  <button type="button" class="btn btn-danger btn-block" v-on:click="register">
+                    Заретстрироваться
+                    </button>
+                </form>
+            </div>
+        </div>
+
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+            return {
+                username: "",
+                passwordA: "",
+                passwordB: ""
+            }
+        },
+        methods: {
+            register: function() {
+                if (this.passwordA === this.passwordB) {
+
+                }
+            }
+        }
+    }
+</script>
